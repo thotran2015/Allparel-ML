6d4c2f6e5e8ddd3903962e6b4a2a6405fa0689c4
Ryan Senanayake <rsenapps@gmail.com>
2018-06-25 00:07:59 -0700

    model set up for training. fixed data loader and configured masked training

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from comet_ml import Experiment 
-from data_loader.simple_mnist_data_loader import SimpleMnistDataLoader
-from models.simple_mnist_model import SimpleMnistModel
+from data_loader.dataloader import DataLoader
+from models.vgg_model import VGGModel
 from trainers.trainer import ModelTrainer
 from utils.config import process_config
 from utils.dirs import create_dirs
@@ -9,21 +9,17 @@ from utils.utils import get_args
 def main():
     # capture the config path from the run arguments
     # then process the json configuration file
-    try:
-        args = get_args()
-        config = process_config(args.config)
-    except:
-        print("missing or invalid arguments")
-        exit(0)
-
+    args = get_args()
+    config = process_config(args.config)
+    
     # create the experiments dirs
     create_dirs([config.callbacks.tensorboard_log_dir, config.callbacks.checkpoint_dir])
 
     print('Create the data generator.')
-    data_loader = SimpleMnistDataLoader(config)
+    data_loader = DataLoader(config)
 
     print('Create the model.')
-    model = SimpleMnistModel(config)
+    model = VGGModel(config)
 
     print('Create the trainer')
     trainer = ModelTrainer(model.model, data_loader.get_train_data(), config)
