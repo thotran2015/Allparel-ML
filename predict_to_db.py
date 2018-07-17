29245776a532a650087c3b510d7f81c9996d17cb
Ryan Senanayake <rsenapps@gmail.com>
2018-07-16 23:36:59 -0700

    process all

--- predict_to_db.py
+++ predict_to_db.py
@@ -8,6 +8,7 @@ from utils.utils import get_args
 from pymongo import MongoClient
 import numpy as np
 import os
+import math
 
 def main():
     # capture the config path from the run arguments
@@ -27,11 +28,15 @@ def main():
     model = VGGModel(config)
     model.load("/home/allparel/Allparel-ML/experiments/2018-07-13/vgg/checkpoints/vgg-56-0.76.hdf5")
     
+    steps = config.predictor.steps
+
+    if config.predictor.process_all:
+        steps = math.ceil(len(predict_generator.filenames) / config.predictor.batch_size)
     # Load from previously trained 
     print('Start Prediction')
     predictions = model.model.predict_generator(
         generator = predict_generator,
-        steps = config.predictor.steps,
+        steps = steps,
         use_multiprocessing=True,
         workers=6,
     )
@@ -40,23 +45,25 @@ def main():
     db = client.allparel
     collection = db.clothes
     for index, prediction in enumerate(predictions):
+        if index >= len(predict_generator.filenames):
+            break
         filename = os.path.basename(predict_generator.filenames[index])
         filepath = config.data_loader.full_image_dir + filename
         print(filepath)
-        db_record = collection.find_one({'image_file': filepath})
-        predicted_tags = db_record.get("predicted_tags", {})
-        predicted_confidences = db_record.get("predicted_confidences", {})
+        # db_record = collection.find_one({'image_file': filepath})
+        # predicted_tags = db_record.get("predicted_tags", {})
+        # predicted_confidences = db_record.get("predicted_confidences", {})
         
         cls, conf = np.argmax(prediction, axis=-1)
-        predicted_tags[config.predictor.tag_name] = labels[cls]
-        predicted_confidences[config.predictor.tag_name] = conf
+        # predicted_tags[config.predictor.tag_name] = labels[cls]
+        # predicted_confidences[config.predictor.tag_name] = conf
         print("filename: ", 
             filename,
             "prediction: ", prediction,
             "class: ", labels[cls],
             "confidence: ", conf
         )
-        update_dict = {'predicted_tags': predicted_tags, 'predicted_confidences': predicted_confidences}
+        # update_dict = {'predicted_tags': predicted_tags, 'predicted_confidences': predicted_confidences}
         # collection.update(
         #     {'image_file':filepath}, 
         #     {'$set':update_dict}, 
